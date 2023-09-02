02cc6d0e9c79f8b6ae3b8222da3d0e6d3ba9351e
Gabrielly Castilho <gabrielly_castilho@outlook.com>
2023-09-02 20:47:11 -0300

    correcoes no arquivo sql e adicao de dados

--- trabalho1/Bicicletario/db/MySQL/create.sql
+++ trabalho1/Bicicletario/db/MySQL/create.sql
@@ -7,7 +7,7 @@ use bicicletario;
 
 create table Usuario
 (
-	codigo 	varchar(20) not null,
+	codigo 	varchar(18) not null,
 	nome   	varchar(256) not null,
     email  	varchar(256) not null unique,
     senha  	varchar(256) not null,
@@ -18,7 +18,7 @@ create table Usuario
 
 create table Cliente
 (
-    cpf        varchar(20)  not null,
+    cpf        varchar(14)  not null,
     telefone   varchar(256),
     sexo       varchar(256),
     nascimento varchar(10),
@@ -28,7 +28,7 @@ create table Cliente
 
 create table Locadora
 (
-    cnpj      	varchar(14)  not null,
+    cnpj      	varchar(19)  not null,
     cidade 		varchar(256),
     foreign key (cnpj) references Usuario (codigo) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -49,8 +49,8 @@ create table Locacao
     status  varchar(20) not null,
     data    varchar(20) not null, 
     val     varchar(20) not null, 
-    cnpj    varchar(14)  not null,
-    cpf     varchar(20)  not null,
+    cnpj    varchar(18)  not null,
+    cpf     varchar(14)  not null,
     bike_id varchar(20) not null,
     primary key (id), 
     foreign key (cnpj) references Locadora(cnpj), 
@@ -62,20 +62,35 @@ create table Locacao
 
 insert into Usuario(codigo, nome, email, senha, papel) values ('12345','admin', 'admin@hotmail.com', 'admin', 'ADMIN');
 
-insert into Usuario values ('6789', 'usuario','user@hotmail.com', '123', 'CLIENTE');
-insert into Cliente values ('6789', 'nao tem', 'feminino', '21/03/2000');
+-- Clientes
+insert into Usuario values ('468.325.873-40', 'Maria','maria@hotmail.com', 'cliente1', 'CLIENTE');
+insert into Cliente values ('468.325.873-40', '(16)99423-5549', 'Feminino', '21/03/2000');
 
-insert into Usuario values ('6782','usuario', 'user2@hotmail.com', '1234', 'CLIENTE');
-insert into Cliente values ('6782', 'nao tem', 'feminino', '21/03/2000');
+insert into Usuario values ('123.456.789-01', 'João', 'joao@hotmail.com', 'cliente2', 'CLIENTE');
+insert into Cliente values ('123.456.789-01', '(16)91234-5678', 'Masculino', '15/07/1995');
 
-insert into Usuario values ('0000','locadora', 'locadora@hotmail.com', '111', 'LOCADORA');
-insert into Locadora values ('0000','são carlos');
+insert into Usuario values ('987.654.321-09', 'Ana', 'ana@hotmail.com', 'cliente3', 'CLIENTE');
+insert into Cliente values ('987.654.321-09', '(16)92345-6789', 'Feminino', '02/11/1988');
 
-insert into Usuario values ('6781','usuario','user1@hotmail.com', '1234',  'LOCADORA');
-insert into Locadora values ('6781','são carlos');
+insert into Usuario values ('567.890.123-45', 'Pedro', 'pedro@hotmail.com', 'cliente4', 'CLIENTE');
+insert into Cliente values ('567.890.123-45', '(16)93456-7890', 'Masculino', '10/05/1993');
 
+-- Locadoras
+insert into Usuario values ('12.345.678/0001-90','Locadora 1', 'locadora1@hotmail.com', 'locadora1', 'LOCADORA');
+insert into Locadora values ('12.345.678/0001-90','São Carlos');
+
+insert into Usuario values ('98.765.432/0001-21', 'Locadora 2', 'locadora2@hotmail.com', 'locadora2', 'LOCADORA');
+insert into Locadora values ('98.765.432/0001-21', 'Campinas');
+
+insert into Usuario values ('45.678.901/0001-34', 'Locadora 3', 'locadora3@hotmail.com', 'locadora3', 'LOCADORA');
+insert into Locadora values ('45.678.901/0001-34', 'Ribeirão Preto');
+
+insert into Usuario values ('23.456.789/0001-45', 'Locadora 4', 'locadora4@hotmail.com', 'locadora4', 'LOCADORA');
+insert into Locadora values ('23.456.789/0001-45', 'Araraquara');
+
+-- Locaçoes
 insert into Bicicleta(id, modelo, ano, descricao, valor) values ('0000', 'modelo', '2013', 'bike', '2000');
-insert into Locacao values ('1111', 'uso finalizado', '21/03/2000', '21.88', '0000', '6789', '0000');
+insert into Locacao values ('1111', 'uso finalizado', '21/03/2000', '21.88', '12.345.678/0001-90', '468.325.873-40', '0000');
 
 insert into Bicicleta(id, modelo, ano, descricao, valor) values ('1111', 'model', '2012', 'bicicleta rosa', '1030');
-insert into Locacao values ('2222', 'em uso', '18/07/2003', '42.47', '6781', '6789', '0000');
+insert into Locacao values ('2222', 'em uso', '18/07/2003', '42.47', '12.345.678/0001-90', '468.325.873-40', '0000');
